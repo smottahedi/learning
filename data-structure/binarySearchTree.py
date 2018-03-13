b8ca11b81970d88fa6e9cd36a261e2bc074a16ae
Sam Mottahedi <sammottahedi@gmail.com>
2018-03-12 23:50:19 -0400

    add bst

--- data-structure/binarySearchTree.py
+++ data-structure/binarySearchTree.py
@@ -31,6 +31,39 @@ class BinaryTree(object):
         return traversal
 
 
+class BST(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def insert(self, new_val):
+        self.insert_helper(self.root, new_val)
+
+    def insert_helper(self, current, new_val):
+        if current.value < new_val:
+            if current.right:
+                self.insert_helper(current.right, new_val)
+            else:
+                current.right = Node(new_val)
+        else:
+            if current.left:
+                self.insert_helper(current.left, new_val)
+            else:
+                current.left = Node(new_val)
+
+    def search(self, find_val):
+        return self.search_helper(self.root, find_val)
+
+    def search_helper(self, current, find_val):
+        if current:
+            if current.value == find_val:
+                return True
+            elif current.value < find_val:
+                return self.search_helper(current.right, find_val)
+            else:
+                return self.search_helper(current.left, find_val)
+        return False
+
+
 if __name__ == "__main__":
 
     # Set up tree
@@ -49,3 +82,18 @@ if __name__ == "__main__":
     # Test print_tree
     # Should be 1-2-4-5-3
     print(tree.print_tree())
+
+    # Set up tree
+    tree = BST(4)
+
+    # Insert elements
+    tree.insert(2)
+    tree.insert(1)
+    tree.insert(3)
+    tree.insert(5)
+
+    # Check search
+    # Should be True
+    print(tree.search(4))
+    # Should be False
+    print(tree.search(6))
